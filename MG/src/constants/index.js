e35b897378f6127a25d83d5c562d2cda22248ba2
MONI5HA <monishagovind2000@gmail.com>
2024-08-19 03:18:24 -0400

    first commit

---
+++ MG/src/constants/index.js
@@ -0,0 +1,150 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/download.jpeg";
+import project3 from "../assets/projects/project-3.jpg";
+
+import project4 from "../assets/projects/project4.png";
+import image from "../assets/projects/image.png";
+
+export const HERO_CONTENT = `As a dynamic software developer, I specialize in Full Stack Web Development and Android Development, merging technology with creativity to craft innovative, user-focused solutions. With a solid foundation in web technologies, and Android SDK, I excel in building responsive, efficient web and mobile applications that exceed user expectations.
+
+My journey includes not only extensive hands-on experience but also active participation in various hackathons, where I’ve secured top positions and refined my problem-solving skills. This competitive edge fuels my passion for continuous learning and pushes me to stay ahead of industry trends.
+
+I thrive on collaborating with forward-thinking teams and am always eager to take on new challenges that push the boundaries of technology and design. Let’s connect and explore how we can drive digital transformation together!`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const experience = [
+  {
+    year: "2021 - 2023",
+    role: "Program Analyst/Data Analyst",
+    company: "Cognizant Technology Solution, Chennai, India.",
+    description:
+      "Created visualizations to tell stories and communicate findings to various stakeholders.Executed Sales Analytics projects by generating Machine Learning-driven dashboards and evaluating performance metrics such as sales representative effectiveness, product offering, activity engagement, and pipeline analysis.Architected and maintaining ETL data flows for a massive Salesforce database, including multiple orgs and sandboxes, incorporating features such as data filters, control access, complex field mappings, and data validation.Optimized data flow, enriching efficiency and reducing run time by 50% (30 minutes).Authored complex SAQL-powered lenses to support multiple business unit analysis needs.",
+    technologies: [
+      "Salesforce",
+      "Tableau",
+      "Python",
+      "SQL",
+      "ETL",
+      "Machine Learning",
+      "Salesforce Analytics Cloud",
+      "Salesforce Einstein Analytics",
+    ],
+  },
+];
+export const education = [
+  {
+    year: "2024 - Present",
+    role: "Masters in Applied Computing",
+    company: "The University of windsor,Canada.",
+    description:
+      "Master of Applied Computing is a professional program that  provided me with a solid foundation of the industry oriented practical aspects of computer science. It provides a solid background in the foundation of computing and business",
+    technologies: [
+      "Java",
+      "C++",
+      "Python",
+      "Data Structures",
+      "Algorithms",
+      "Database Management Systems",
+      "Software Engineering",
+      "Web Development",
+      "Mobile Development",
+      "Cloud Computing",
+      "Big Data",
+      "Machine Learning",
+      "Artificial Intelligence",
+      "Cybersecurity",
+      "Human-Computer Interaction",
+      "Project Management",
+      "Business Analysis",
+    ],
+  },
+  {
+    year: "2022 - 2024",
+    role: "Master of Science in Augment Reality and Virtual Reality",
+    company: "Indian Institute of Technology, Jodhpur, India",
+    description:
+      "The M.Sc. in Augmented Reality and Virtual Reality is a two-year postgraduate program that provides me with a comprehensive understanding of the principles and practices of AR and VR technologies. The program covers a wide range of topics, including computer graphics, human-computer interaction, and 3D modeling. I learnt how to design and develop AR and VR applications for a variety of industries, including gaming, entertainment, education, and healthcare.",
+    technologies: ["Mobile AR and VR", "Haptics", "Sensation and Perception"],
+  },
+  {
+    year: "2022 - 2024",
+    role: "Master of Business Administration in Business Analytics",
+    company: "Anna University, Chennai, India",
+    description:
+      "The MBA in Business Analytics is a two-year postgraduate program that provides me with a solid foundation in business analytics and data science. The program covers a wide range of topics, including data mining, machine learning, and predictive analytics. I learnt how to use data to drive business decisions and improve organizational performance.",
+    technologies: [
+      "Python",
+      "Big Data Analytics",
+      "Human Resource Management",
+      "Machine Learning",
+    ],
+  },
+  {
+    year: "2017 - 2021",
+    role: "Bachelor of Engineering in Computer Science & Engineering",
+    company: "Anna University,Chennai, India",
+    description:
+      "The Bachelor of Engineering in Computer Science & Engineering is a four-year undergraduate program that provides me with a solid foundation in computer science and engineering. The program covers a wide range of topics, including programming, data structures, algorithms, and computer networks. I learnt how to design and develop software applications, manage IT projects, and solve complex problems using technology.",
+    technologies: [
+      "Data Structure",
+      "Artificial Intelligence ",
+      "Software Engineering",
+    ],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "Paving Concepts",
+    image: image,
+    description:
+      "The website was built to address the growing demand for reliable and professional paving services. It was designed to attract new customers, provide them with detailed information about available services, and establish a strong online presence for the business",
+    technologies: [
+      "HTML",
+      "CSS",
+      "React",
+      "Node.js",
+      "React JS",
+      "Google Domains",
+      "GCP",
+    ],
+  },
+  {
+    title: "Interactive Human Liver System",
+    image: project2,
+    description:
+      "Constructed an interactive human liver system using Unity, designed to advance educational experiences through immersive 3D visualization.Utilized Unity's AR capabilities to ensure smooth integration and high-quality performance on various AR devices, improving user engagement and learning outcomes.Developed a user-friendly interface that allows users to explore the human liver system in detail, including its structure, functions, and common diseases",
+    technologies: ["Unity (for AR development)", " C#", " Blender"],
+  },
+  {
+    title: "Impact of AI Adoption on Business Performance Dashboard",
+    image: project3,
+    description:
+      "	Designed and distributed a comprehensive questionnaire, employing exploratory factor analysis, KMO, Bartlett’s test, and correlation analysis to pinpoint key performance drivers, Upgrading data interpretation accuracy by 70%.",
+    technologies: [
+      "Power BI",
+      "Python (for data analysis and exploratory techniques)",
+      "Heroku.",
+    ],
+  },
+  {
+    title: "PodcastHM",
+    image: project4,
+    description:
+      "Here News is taken as the example of the data to be streamed and the news articles are the data source and an optimized and more personalized specific to subscribed topics, audio is generated using modern technology which includes Artificial Intelligence. Here the audio acts as a 2-minute podcast that explains what has happened around that day about those topics as a summarized version.",
+    technologies: [
+      "Android Studio",
+      "LLM",
+      "Firebase.",
+      "Java",
+      "Machine Learning",
+    ],
+  },
+];
+
+export const CONTACT = {
+  address: "Windsor, Ontario, Canada",
+  phoneNo: "+1(382) 880 -0606 ",
+  email: "monishagovind2000@gmail.com",
+};
